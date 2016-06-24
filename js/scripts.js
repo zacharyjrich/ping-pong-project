025e481cda9a5230cc06f666e3a123410cacd0d1
Zachary Richards <zachary.j.richards@gmail.com>
2016-06-23 21:40:18 -0700

    add business and front end js to get first spec to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
+var listNumbers = function(input) {
+  var numbers = [];
+  for (var i = 1; i <= input; i ++) {
+    numbers.push(i);
+
+  }
+   return numbers;
+};
+
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#number_input").submit(function(event) {
+    event.preventDefault();
 
+    var input = parseInt($("input#submission").val());
+    var result = listNumbers(input);
 
+    // $("#output").text(result);
+    result.forEach(function(listResult) {
+    $("#output").append("<li>" + listResult + "</li>");
+    });
   });
 });
